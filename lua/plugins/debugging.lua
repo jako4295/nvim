1e4d5e45dec765a6678c8c12c6ec48770fe18326
Jakob Olsen <jols19@student.aau.dk>
2024-06-05 18:44:35 +0200

    Improved python workflow including debugging

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -11,16 +11,15 @@ return {
 
         require("dap-python").setup("~/.config/nvim/debugpy_venv/bin/python")
 
-        dap.listeners.before.attach.dapui_config = function()
+        dapui.setup()
+
+        dap.listeners.after.event_initialized["dapui_config"] = function()
             dapui.open()
         end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
+        dap.listeners.before.event_terminated["dapui_config"] = function()
             dapui.close()
         end
-        dap.listeners.before.event_exited.dapui_config = function()
+        dap.listeners.before.event_exited["dapui_config"] = function()
             dapui.close()
         end
 
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,32 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local autogroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.prettier,
-            },
-        })
-
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black, -- For python
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.prettier,
+			},
+			on_attach = function(client, bufnr) -- Auto format on save for any formatter
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = autogroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = autogroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
